6485b56f8c181c9145ffbf46df8d3d1033627290
Nhat Quang Bui <58140652+Kinmango@users.noreply.github.com>
2023-04-29 20:06:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from website import create_app
 
 app = create_app()
 
-@app.route('/')
-def login():
-    return render_template('login.html')
-
 # This if statement means we only run the server when we run this file instead of just importing this file
 if __name__ == '__main__':
     # The argument debug True means it automatically rerun the webserver every time we make a change to our code
